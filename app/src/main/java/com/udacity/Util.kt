94c50c3a55462625533f8c4d386f5e3e84c49ce1
hoangbach <petxuanbach@gmail.com>
2023-07-02 17:07:43 +0700

    Third commit

---
+++ app/src/main/java/com/udacity/Util.kt
@@ -0,0 +1,11 @@
+package com.udacity
+
+class Util {
+    companion object {
+        val URL1 = "https://github.com/bumptech/glide"
+        val URL2 =
+            "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter"
+        val URL3 = "https://github.com/square/retrofit"
+    }
+
+}
